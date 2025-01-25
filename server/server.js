5b4537dcaa02f9c35eb112734ac29ea410f8a0f3
ermcgonagle <ermcgonagle>
2025-01-25 16:31:05 -0600

    setting up backend

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// const { Pool } = require('pg');
+
+// // Create a connection pool to PostgreSQL
+// const pool = new Pool({
+//   user: 'ermcgonagle    ',
+//   host: 'localhost', // or your database server address
+//   database: 'ticketpull_database',
+//   password: 'newpassword',
+//   port: 5432, // Default PostgreSQL port
+// });
+
+// module.exports = pool;
