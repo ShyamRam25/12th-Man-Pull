ac3d1563b5737a10088ee1b0c2e82c195a0ce94e
Andrew Lin <andrew.mlin27@gmail.com>
2025-01-26 06:51:28 -0600

    give uin on errors

--- server/helpers.js
+++ server/helpers.js
@@ -5,16 +5,18 @@ const validate_uin = async (uin) => {
     var result1 = await pool.query('SELECT EXISTS (SELECT 1 FROM students WHERE uin = $1)', [uin]);
     exists = result1.rows[0].exists;
     if (!exists) {
-        const error = new Error('UIN does not exist');
+        const error = new Error(`UIN ${uin} does not exist`);
         error.statusCode = 401;
+        error.uin = uin;
         throw error;
     }
 
     var result2 = await pool.query('SELECT is_pulled FROM students WHERE uin = $1', [uin]);
     pulled = result2.rows[0].is_pulled;
     if (pulled) {
-        const error = new Error('Ticket already pulled');
+        const error = new Error(`Ticket already pulled for UIN ${uin}`);
         error.statusCode = 402;
+        error.uin = uin;
         throw error;
     }
 
