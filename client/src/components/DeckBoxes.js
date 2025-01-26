e2615909242369c33a270347fae3c3b6dc3eabb9
ermcgonagle <ermcgonagle>
2025-01-26 08:06:18 -0600

    adjusted modal pop up

--- client/src/components/DeckBoxes.js
+++ client/src/components/DeckBoxes.js
@@ -9,6 +9,8 @@ const DeckBoxes = () => {
   const [showPopup, setShowPopup] = useState(false); // State to show/hide the pop-up
   const [validationMessage, setValidationMessage] = useState('');
   const [res_names, setResNames] = useState([]);
+  const [selectedModal, setSelectedModal] = useState("default");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleValidate = async () => {
     console.log(sportsPasses);
@@ -19,10 +21,30 @@ const DeckBoxes = () => {
       var api_string = "http://localhost:3001/api/check-classifications?uins=" + uins;
       const res = await axios.get(api_string);
       setResNames(res.data.names);
+      console.log(res.status);
+      if (res.status === 200) {
+        showDefaultView();
+      }
       setShowPopup(true);
     } catch (err) {
-      console.error("Couldn't validate sports passes:", err);
+      console.error("Couldn't validate sports passes:", err.status);
+      if (err.status === 403) {
+        showErrorView("Invalid Ratio");
+      } else if (err.status === 401) {
+        showErrorView("Invalid UIN");
+      } else if (err.status === 402) {
+        showErrorView("Already Pulled");
+      } else {
+        showErrorView("Error");
+      }
+      setShowPopup(true);
     }
+  }
+
+  const showDefaultView = () => setSelectedModal("default");
+  const showErrorView = (message) => {
+    setSelectedModal("error");
+    setErrorMessage(message);
   }
 
   const handleDeckClick = (deckIndex) => {
@@ -76,15 +98,14 @@ const DeckBoxes = () => {
             <div
               key={index}
               onClick={() => handleDeckClick(index)} // Handle click on deck box
-              className={`w-full h-40 flex justify-center items-center text-aggie-white font-bold text-2xl rounded-lg cursor-pointer border border-black transition-all duration-300 ease-in-out ${
-                isValidated
-                  ? activeDeck === index
-                    ? 'bg-aggie-maroon hover:bg-maroon-dark'
-                    : 'bg-aggie-gray hover:bg-maroon-dark'
-                  : activeDeck === index
+              className={`w-full h-40 flex justify-center items-center text-aggie-white font-bold text-2xl rounded-lg cursor-pointer border border-black transition-all duration-300 ease-in-out ${isValidated
+                ? activeDeck === index
+                  ? 'bg-aggie-maroon hover:bg-maroon-dark'
+                  : 'bg-aggie-gray hover:bg-maroon-dark'
+                : activeDeck === index
                   ? 'bg-aggie-maroon hover:bg-maroon-dark'
                   : 'bg-other-white hover:bg-maroon-dark'
-              }`}
+                }`}
             >
               {deck}
             </div>
@@ -93,11 +114,10 @@ const DeckBoxes = () => {
           {/* Pull Tickets Button */}
           <button
             onClick={handlePullTickets}
-            className={`${
-              activeDeck === null
-                ? 'bg-gray-500 cursor-not-allowed'
-                : 'bg-aggie-maroon hover:bg-aggie-white hover:text-aggie-maroon'
-            } text-other-white text-4xl px-12 py-3 rounded-md font-custom-font transition-all mt-8`}
+            className={`${activeDeck === null
+              ? 'bg-gray-500 cursor-not-allowed'
+              : 'bg-aggie-maroon hover:bg-aggie-white hover:text-aggie-maroon'
+              } text-other-white text-4xl px-12 py-3 rounded-md font-custom-font transition-all mt-8`}
             disabled={activeDeck === null} // Disable button if no deck is selected
           >
             Pull Tickets
@@ -106,7 +126,8 @@ const DeckBoxes = () => {
       </div>
 
       {/* Pop-up with Sports Passes */}
-      {showPopup && (
+      {/* {showPopup && ( */}
+      {selectedModal === "default" && showPopup && (
         <div
           className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center"
           onClick={closePopup} // Close on outside click
@@ -122,6 +143,31 @@ const DeckBoxes = () => {
                   {name}
                 </li>
               ))}
+            </ul>
+            <button
+              onClick={closePopup}
+              className="bg-aggie-maroon font-custom-font text-aggie-white py-2 px-4 rounded-lg mt-4 hover:bg-maroon-dark transition-all" // Close button
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      )}
+
+      {selectedModal === "error" && showPopup && (
+        <div
+          className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center"
+          onClick={closePopup} // Close on outside click
+        >
+          <div
+            className="bg-aggie-white p-6 rounded-lg w-72 text-center"
+            onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
+          >
+            <h3 className="text-xl font-custom-font font-semibold mb-4">Error</h3>
+            <ul className="list-none p-0">
+              <li>
+                {errorMessage}
+              </li>
             </ul>
             <button
               onClick={closePopup}
