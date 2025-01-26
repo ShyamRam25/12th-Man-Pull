21dfa0ba735bb23de6a31b5cca5d803b8c15b9e5
Andrew Lin <andrew.mlin27@gmail.com>
2025-01-26 05:01:30 -0600

    check_classification done

---
+++ server/controllers.js
@@ -0,0 +1,50 @@
+const pool = require('./db');
+const { validate_uin } = require('./helpers');
+
+const getStudents = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM students');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error querying students:', err);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+};
+
+const checkClassifications = async (req, res) => {
+    const uins = req.query.uins?.split(',');
+    var over = 0;
+    var under = 0;
+    var names = [];
+
+    try {
+        for (const uin of uins) {
+            if (await validate_uin(uin)) {
+                over += 1;
+            }
+            else {
+                under += 1;
+            }
+            result = await pool.query('SELECT name FROM students WHERE uin = $1', [uin]);
+            names.push(result.rows[0].name);
+        }
+        
+        if (under > over) {
+            res.status(403).json({ error: 'Invalid ratio', over, under, names});
+            return;
+        }
+        res.json({ over, under, names });
+    } catch (err) { 
+        res.status(err.statusCode || 500).json({ error: err.message || 'internal error'});
+    }
+};
+
+const checkDecks = async (req, res) => {
+
+};
+
+const pullTickets = async (req, res) => {
+
+};
+
+module.exports = { getStudents, checkClassifications, checkDecks, pullTickets };
---
+++ server/helpers.js
@@ -0,0 +1,29 @@
+const pool = require('./db');
+
+// check uin exists, classification T/F, is_pulled T/F
+const validate_uin = async (uin) => {
+    var result1 = await pool.query('SELECT EXISTS (SELECT 1 FROM students WHERE uin = $1)', [uin]);
+    exists = result1.rows[0].exists;
+    if (!exists) {
+        const error = new Error('UIN does not exist');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    var result2 = await pool.query('SELECT is_pulled FROM students WHERE uin = $1', [uin]);
+    pulled = result2.rows[0].is_pulled;
+    if (pulled) {
+        const error = new Error('Ticket already pulled');
+        error.statusCode = 402;
+        throw error;
+    }
+
+    var result3 = await pool.query('SELECT classification FROM students WHERE uin = $1', [uin]);
+    classification = result3.rows[0].classification;
+    if (classification < 4) {
+        return false;
+    }
+    return true;
+};
+
+module.exports = { validate_uin };
