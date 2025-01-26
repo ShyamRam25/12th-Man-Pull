7be81338d0b58d65bb5795043678478c9ca8e5ff
Shyam Ramachandran <janshyam03@gmail.com>
2025-01-26 00:44:38 -0600

    Fixed NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="bg-aggie-maroon flex fixed w-screen h-20 items-center">
       {/* Left side: About and Home links */}
       <div className="flex-1 flex justify-center mr-auto">
-        <Link to="/about" className="text-aggie-white font-custom-font text-2xl mx-96">About</Link>
+        <Link to="/about" className="text-aggie-white font-custom-font text-2xl mx-80">About</Link>
         <Link to="/" className="text-aggie-white font-custom-font text-2xl -mx-56">Home</Link>
       </div>
 
@@ -18,8 +18,8 @@ const Navbar = () => {
 
       {/* Right side: Dashboard and FAQ links */}
       <div className="flex-1 flex justify-center ml-auto">
-        <Link to="/dashboard" className="text-aggie-white font-custom-font text-2xl -mx-56">Profile Dashboard</Link>
-        <Link to="/FAQ" className="text-aggie-white font-custom-font text-2xl mx-96">FAQ</Link>
+        <Link to="/dashboard" className="text-aggie-white font-custom-font text-2xl -mx-56">Profile</Link>
+        <Link to="/FAQ" className="text-aggie-white font-custom-font text-2xl mx-80">FAQ</Link>
       </div>
 
       
